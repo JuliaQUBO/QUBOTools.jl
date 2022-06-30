e428db602671a2a5918ae4d3d33c27e6458ed648
pedromxavier <pedromxavier@poli.ufrj.br>
2022-06-30 00:26:19 -0300

    Add `include` calls

--- src/BQPIO.jl
+++ src/BQPIO.jl
@@ -3,6 +3,10 @@ module BQPIO
 using JSON
 
 include("interface.jl")
-include("models/bqpjson.jl")
+include(joinpath("models", "bqpjson.jl"))
+include(joinpath("models", "hfs.jl"))
+include(joinpath("models", "minizinc.jl"))
+include(joinpath("models", "qubist.jl"))
+include(joinpath("models", "qubo.jl"))
 
 end # module
