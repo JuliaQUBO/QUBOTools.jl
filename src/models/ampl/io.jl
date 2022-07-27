a597ca6e4aba614a6bb7afd0588f022acaf1ff98
pedromxavier <pedromxavier@poli.ufrj.br>
2022-07-26 22:53:25 -0300

    Add AMPL I/O

--- src/models/ampl/io.jl
+++ src/models/ampl/io.jl
@@ -1,54 +1,28 @@
 # ~*~ I/O ~*~ #
-function Base.read(::IO, M::Type{<:AbstractBQPModel})
-    bqpcodec_error("'Base.read' not implemented for model of type '$(M)'")
-end
+function Base.write(mod_io::IO, model::AMPL)
+    N = BQPIO.domain_size(model)
 
-function Base.read(path::AbstractString, M::Type{<:AbstractBQPModel})
-    open(path, "r") do io
-        return read(io, M)
+    obj_terms = []
+
+    for (i, l) in BQPIO.linear_terms(model)
+        push!(obj_terms, "$(l)*x[$(i)]")
     end
-end
 
-function Base.write(::IO, model::AbstractBQPModel)
-    bqpcodec_error("'Base.write' not implemented for model of type '$(typeof(model))'")
-end
-
-function Base.write(path::AbstractString, model::AbstractBQPModel)
-    open(path, "w") do io
-        return write(io, model)
+    for ((i, j), q) in BQPIO.quadratic_terms(model)
+        push!(obj_terms, "$(q)*x[$(i)]*x[$(j)]")
     end
-end
 
-function Base.convert(M::Type{<:AbstractBQPModel}, model::AbstractBQPModel)
-    bqpcodec_error("'Base.convert' not implemented for turning model of type '$(typeof(model))' into $(M)")
-end
+    obj_expr = join(obj_terms, "+")
 
-function Base.convert(::Type{M}, model::M) where {M<:AbstractBQPModel}
-    model # Short-circuit! Yeah!
-end
-
-function Base.show(io::IO, model::AbstractBQPModel)
     print(
-        io,
+        mod_io,
         """
-        $(BQPIO.model_name(model)) Model:
-        $(BQPIO.domain_size(model)) variables [$(BQPIO.domain_name(model))]
+        var x {1..$(N)} binary;
 
-        Density:
-        linear    ~ $(@sprintf("%0.2f", 100.0 * BQPIO.linear_density(model)))%
-        quadratic ~ $(@sprintf("%0.2f", 100.0 * BQPIO.quadratic_density(model)))%
-        total     ~ $(@sprintf("%0.2f", 100.0 * BQPIO.density(model)))%
+        minimize objective:
+            $(obj_expr);
         """
     )
 end
 
-function Base.copy!(::M, ::M) where {M<:AbstractBQPModel}
-    bqpcodec_error("'Base.copy!' not implemented for copying '$M' in-place")
-end
-
-function Base.copy!(
-    target::X,
-    source::Y,
-) where {X<:AbstractBQPModel,Y<:AbstractBQPModel}
-    copy!(target, convert(typeof(target), source))
-end
+BQPIO.infer_model_type(::Val{:mod}) = AMPL
