350b55e46e5260d5c24945b86361de1c27f66d63
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-08-19 17:55:52 -0300

    Fix schema path case

--- src/models/bqpjson/bqpjson.jl
+++ src/models/bqpjson/bqpjson.jl
@@ -1,17 +1,14 @@
-const BQPJSON_SCHEMA = JSONSchema.Schema(JSON.parsefile(joinpath(@__DIR__, "BQPJSON.schema.json")))
-const BQPJSON_VERSION_LIST = VersionNumber[v"1.0.0"]
-const BQPJSON_VERSION_LATEST = BQPJSON_VERSION_LIST[end]
+const BQPJSON_SCHEMA          = JSONSchema.Schema(JSON.parsefile(joinpath(@__DIR__, "bqpjson.schema.json")))
+const BQPJSON_VERSION_LIST    = VersionNumber[v"1.0.0"]
+const BQPJSON_VERSION_LATEST  = BQPJSON_VERSION_LIST[end]
 const BQPJSON_BACKEND_TYPE{D} = StandardQUBOModel{Int,Int,Float64,D}
 
-function BQPJSON_VARIABLE_DOMAIN end
 BQPJSON_VARIABLE_DOMAIN(::Type{<:BoolDomain}) = "boolean"
 BQPJSON_VARIABLE_DOMAIN(::Type{<:SpinDomain}) = "spin"
 
-function BQPJSON_VALIDATE_DOMAIN end
 BQPJSON_VALIDATE_DOMAIN(x::Integer, ::Type{<:BoolDomain}) = x == 0 || x == 1
 BQPJSON_VALIDATE_DOMAIN(s::Integer, ::Type{<:SpinDomain}) = s == -1 || s == 1
 
-function BQPJSON_SWAP_DOMAIN end
 BQPJSON_SWAP_DOMAIN(x::Integer, ::Type{<:BoolDomain}) = (x == 1 ? 1 : -1)
 BQPJSON_SWAP_DOMAIN(s::Integer, ::Type{<:SpinDomain}) = (s == 1 ? 1 : 0)
 
@@ -48,50 +45,6 @@ BQPJSON_SWAP_DOMAIN(s::Integer, ::Type{<:SpinDomain}) = (s == 1 ? 1 : 0)
 
         BQPJSON{D}(backend; solutions=solutions)
     end
-end
-
-function QUBOTools.__isvalidbridge(
-    source::BQPJSON{B},
-    target::BQPJSON{B},
-    ::Type{<:BQPJSON{A}};
-    kws...
-) where {A,B}
-    flag = true
-
-    if QUBOTools.id(source) != QUBOTools.id(target)
-        @error "Test Failure: ID mismatch"
-        flag = false
-    end
-
-    if QUBOTools.version(source) != QUBOTools.version(target)
-        @error "Test Failure: Version mismatch"
-        flag = false
-    end
-
-    if !isnothing(QUBOTools.description(source)) && (QUBOTools.description(source) != QUBOTools.description(target))
-        @error "Test Failure: Description mismatch"
-        flag = false
-    end
-
-    if !isempty(QUBOTools.metadata(source)) && (QUBOTools.metadata(source) != QUBOTools.metadata(target))
-        @error "Test Failure: Inconsistent metadata"
-        flag = false
-    end
-
-    if !isnothing(source.solutions) && (source.solutions != source.solutions)
-        @error "Test Failure: Inconsistent solutions"
-        flag = false
-    end
-
-    if !QUBOTools.__isvalidbridge(
-        QUBOTools.backend(source),
-        QUBOTools.backend(target);
-        kws...
-    )
-        flag = false
-    end
-
-    return flag
 end
 
 include("data.jl")
--- src/models/bqpjson/data.jl
+++ src/models/bqpjson/data.jl
@@ -10,3 +10,48 @@ function QUBOTools.version(model::BQPJSON)
         return BQPJSON_VERSION_LATEST
     end
 end
+
+function QUBOTools.__isvalidbridge(
+    source::BQPJSON{B},
+    target::BQPJSON{B},
+    ::Type{<:BQPJSON{A}};
+    kws...
+) where {A,B}
+    flag = true
+
+    if QUBOTools.id(source) != QUBOTools.id(target)
+        @error "Test Failure: ID mismatch"
+        flag = false
+    end
+
+    if QUBOTools.version(source) != QUBOTools.version(target)
+        @error "Test Failure: Version mismatch"
+        flag = false
+    end
+
+    if QUBOTools.description(source) != QUBOTools.description(target)
+        @error "Test Failure: Description mismatch"
+        flag = false
+    end
+
+    if QUBOTools.metadata(source) != QUBOTools.metadata(target)
+        @error "Test Failure: Inconsistent metadata"
+        flag = false
+    end
+
+    # TODO: How to compare them?
+    # if source.solutions != target.solutions
+    #     @error "Test Failure: Inconsistent solutions"
+    #     flag = false
+    # end
+
+    if !QUBOTools.__isvalidbridge(
+        QUBOTools.backend(source),
+        QUBOTools.backend(target);
+        kws...
+    )
+        flag = false
+    end
+
+    return flag
+end
