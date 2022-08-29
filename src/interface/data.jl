2ce5014e797bfe5aff1180dd05e1ca7d219adfc5
pedromxavier <pedromxavier@poli.ufrj.br>
2022-08-29 09:38:39 -0300

    Add docs to interface

--- src/interface/data.jl
+++ src/interface/data.jl
@@ -6,107 +6,121 @@
 """
 
 @doc raw"""
-    backend(model)
+    backend(model)::AbstractQUBOModel
+    backend(model::AbstractQUBOModel)::AbstractQUBOModel
 
+Retrieves the model's backend.
+Implementing this function allows one to profit from fallback implementations of the other methods.
 """ function backend end
 
+# ~*~ Data access ~*~ #
 @doc raw"""
-    model_name(model)
+    model_name(model)::String
 
+Returns a string representing the model type.
 """ function model_name end
 
 @doc raw"""
-    domain(model)
+    domain(model)::VariableDomain
 
+Returns the type representing the variable domain of a given model.
 """ function domain end
 
 @doc raw"""
-    swap_domain(model)
-    
-""" function swap_domain end
+    domain_name(model)::String
 
-@doc raw"""
-    domain_name(model)
-
+Returns a string representing the variable domain.
 """ function domain_name end
 
 @doc raw"""
-    sense(model)
+    swap_domain(::Type{B}, model::AbstractQUBOModel{A}) where {A<:VariableDomain, B<:VariableDomain}
 
+Returns a new model, switching from domain `A` to domain `B`.
+""" function swap_domain end
+
+@doc raw"""
+    sense(model)::Symbol
+
+Returns the optimization sense of a model, as a symbol (:min or :max)
 """ function sense end
 
 @doc raw"""
-    swap_sense(model)
+    swap_sense(model::AbstractQUBOModel)::AbstractQUBOModel
     
+Returns a new model, inverting the optimization sense (:min or :max)
 """ function swap_sense end
 
 @doc raw"""
-    offset(model)
+    offset(model)::T where {T <: Real}
 
 """ function offset end
 
 @doc raw"""
-    scale(model)
+    scale(model)::T where {T <: Real}
 
 """ function scale end
 
 @doc raw"""
-    id(model)
+    id(model)::Integer
 """ function id end
 
 @doc raw"""
-    version(model)
+    version(model)::Union{VersionNumber, Nothing}
 """ function version end
 
 @doc raw"""
-    description(model)
+    description(model)::Union{String, Nothing}
 """ function description end
 
 @doc raw"""
-    metadata(model)
+    metadata(model)::Dict{String, Any}
 """ function metadata end
 
 @doc raw"""
-    sampleset(model)
+    sampleset(model)::QUBOTools.SampleSet
 """ function sampleset end
 
 @doc raw"""
-    linear_terms(model)
-    linear_terms(model; explicit::Bool)
+    linear_terms(model; explicit::Bool = false)::Dict{Int,T} where {T <: Real}
 
 Retrieves the linear terms of a model as a dict.
 
-The `explicit` keyword includes all variables, breaking sparsity.
+The `explicit` keyword includes all variables, breaking linear sparsity.
 
 """ function linear_terms end
 
 @doc raw"""
+    quadratic_terms(model)::Dict{Tuple{Int,Int},T} where {T <: Real}
+
+Retrieves the quadratic terms of a model as a dict.
+For every key pair ``(i, j)`` holds that ``i < j``.
 """ function quadratic_terms end
 
 @doc raw"""
-    variables(model)
+    variables(model)::Vector
 
-Returns an ordered vector containing the model's variables.
+Returns a sorted vector containing the model's variables.
 If order doesn't matter, use `variable_set(model)` instead.
 """ function variables end
 
 @doc raw"""
-    variable_set(model)
+    variable_set(model)::Set
 
 Returns the set of variables of a given model.
 """ function variable_set end
 
 @doc raw"""
-    variable_map(model)
-    variable_map(model, x)
+    variable_map(model)::Dict{V,Int} where {V}
+    variable_map(model, x::V)::Integer where {V}
 
 """ function variable_map end
 
 @doc raw"""
-    variable_inv(model)
-    variable_inv(model, i)
+    variable_inv(model)::Dict{Int,V} where {V}
+    variable_inv(model, i::Integer)::V where {V}
 """ function variable_inv end
 
+# ~*~ Model's Normal Forms ~*~ #
 @doc raw"""
     qubo(model::AbstractQUBOModel{<:BoolDomain})
     qubo(::Type{<:Dict}, T::Type, model::AbstractQUBOModel{<:BoolDomain})
@@ -142,31 +156,56 @@ Returns a quadruple ``(h, J, \alpha, \beta)`` where:
 * `β::T` is the offset constant.
 """ function ising end
 
+# ~*~ Data queries ~*~ #
 @doc raw"""
-    energy(state, model)
+    energy(state, model)::T where {T<:Real}
 
 This function aims to evaluate the energy of a given state under some QUBO Model.
 **Note:** Scale and offset factors are taken into account.
 """ function energy end
 
+# ~*~ Queries: sizes & density ~*~ #
 @doc raw"""
+    domain_size(model)::Integer
+
+Counts the number of variables in the model.
 """ function domain_size end
 
 @doc raw"""
+    linear_size(model)::Int
+
+Counts the number of non-zero linear terms in the model.
 """ function linear_size end
 
 @doc raw"""
+    quadratic_size(model)::Int
+
+Counts the number of non-zero quadratic terms in the model.
 """ function quadratic_size end
 
 @doc raw"""
+    density(model)::Float64
+
+Computes the density ``\rho`` of non-zero terms in a model, according to the expression
+```math
+\rho = \frac{2Q + L}{N^{2}}
+```
+where ``l`` is the number of linear terms, ``Q`` the number of quadratic ones and ``N`` the number of variables.
+
+If the model is empty, returns `NaN`.
 """ function density end
 
 @doc raw"""
+    linear_density(model)::Float64
+
 """ function linear_density end
 
 @doc raw"""
+    quadratic_density(model)::Float64
+
 """ function quadratic_density end
 
+# ~*~ Internal: bridge validation ~*~ #
 @doc raw"""
     __isvalidbridge(source::M, target::M, ::Type{<:AbstractQUBOModel}; kws...) where M <: AbstractQUBOModel
 
