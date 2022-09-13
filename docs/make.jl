8d2e44d532411b7c0868b71e382794a568ba9f55
Pedro Maciel Xavier <31925649+pedromxavier@users.noreply.github.com>
2022-09-13 17:27:59 -0300

    Update `TeX` engine

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(;
     clean = true,
     format = Documenter.HTML(
         assets = ["assets/extra_styles.css", "assets/favicon.ico"],
-        mathengine = Documenter.MathJax2(),
+        mathengine = Documenter.KaTeX(),
         sidebar_sitename = false,
     ),
     sitename = "QUBOTools.jl",
