0d63ef25a983103c507720aa53e7fd63f341b4d7
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2023-08-25 11:39:52 -0300

    Add comments

--- src/library/format/qubist/parser.jl
+++ src/library/format/qubist/parser.jl
@@ -1,3 +1,33 @@
+function read_model(io::IO, fmt::Qubist)
+    data = Dict{Symbol,Any}(
+        :linear_terms    => Dict{Int,Float64}(),
+        :quadratic_terms => Dict{Tuple{Int,Int},Float64}(),
+    )
+
+    for line in readlines(io)
+        _parse_line!(data, strip(line), fmt)
+    end
+
+    return Model{Int,Float64,Int}(
+        Set{Int}(1:data[:dimension]),
+        data[:linear_terms],
+        data[:quadratic_terms],
+        sense  = :min,
+        domain = :spin,
+    )
+end
+
+function _parse_line!(data::Dict{Symbol,Any}, line::AbstractString, fmt::Qubist)
+    isempty(line) && return nothing
+
+    _parse_entry!(data, line, fmt) && return nothing
+    _parse_header!(data, line, fmt) && return nothing
+
+    syntax_error("'$line'")
+
+    return nothing
+end
+
 function _parse_entry!(data::Dict{Symbol,Any}, line::AbstractString, ::Qubist)
     m = match(r"^\s*([0-9]+)\s+([0-9]+)\s+([+-]?([0-9]*[.])?[0-9]+)\s*$", line)
 
@@ -31,15 +61,11 @@ function _parse_header!(data::Dict{Symbol,Any}, line::AbstractString, ::Qubist)
 
     dimension  = parse(Int, m[1])
     total_size = parse(Int, m[2])
-
-    if isnothing(dimension) || isnothing(total_size)
-        syntax_error("Invalid header: '$line'")
-    end
-
+    
     data[:dimension] = dimension
 
-    # TODO: `sizehint!` linear and quadratic collections
-    # IDEA: 
+    # `sizehint!` linear and quadratic collections for dictionary pre-allocation
+    # Rationale: 
     #   1. linear_size + quadratic_size = total_size
     #   2. dimension * linear_size    ≈ linear_size + 2 quadratic_size
     #   
@@ -52,32 +78,4 @@ function _parse_header!(data::Dict{Symbol,Any}, line::AbstractString, ::Qubist)
     sizehint!(data[:quadratic_terms], quadratic_size)
 
     return true
-end
-
-function _parse_line!(data::Dict{Symbol,Any}, line::AbstractString, fmt::Qubist)
-    isempty(line) && return nothing
-
-    _parse_entry!(data, line, fmt) && return nothing
-    _parse_header!(data, line, fmt) && return nothing
-
-    syntax_error("'$line'")
-end
-
-function read_model(io::IO, fmt::Qubist)
-    data = Dict{Symbol,Any}(
-        :linear_terms    => Dict{Int,Float64}(),
-        :quadratic_terms => Dict{Tuple{Int,Int},Float64}(),
-    )
-
-    for line in readlines(io)
-        _parse_line!(data, strip(line), fmt)
-    end
-
-    return Model{Int,Float64,Int}(
-        Set{Int}(1:data[:dimension]),
-        data[:linear_terms],
-        data[:quadratic_terms],
-        sense  = :min,
-        domain = :spin,
-    )
 end
