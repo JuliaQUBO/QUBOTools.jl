e4a309d41399e37105591dae7703b27aa6b9fa8c
pedromxavier <pedromxavier@poli.ufrj.br>
2023-09-22 04:36:35 -0400

    Add missing constant definitions

--- ext/QUBOTools_MOI/QUBOTools_MOI.jl
+++ ext/QUBOTools_MOI/QUBOTools_MOI.jl
@@ -3,8 +3,12 @@ module QUBOTools_MOI
 import QUBOTools
 import MathOptInterface as MOI
 
-const MOIU = MOI.Utilities
-const VI = MOI.VariableIndex
+const MOIU   = MOI.Utilities
+const VI     = MOI.VariableIndex
+const SAF{T} = MOI.ScalarAffineFunction{T}
+const SAT{T} = MOI.ScalarAffineTerm{T}
+const SQF{T} = MOI.ScalarQuadraticFunction{T}
+const SQT{T} = MOI.ScalarQuadraticTerm{T}
 
 include("error.jl")
 include("varlt.jl")
