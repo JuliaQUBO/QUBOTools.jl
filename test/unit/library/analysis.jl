4041fbeefd2a95ccdd6992867c018a98c30fb6dd
pedromxavier <pedromxavier@poli.ufrj.br>
2023-09-23 15:29:48 -0400

    Add tests for metrics

--- test/unit/library/analysis.jl
+++ test/unit/library/analysis.jl
@@ -1,58 +1,125 @@
 function test_metrics()
     @testset "→ Metrics" verbose = true begin
-        e = 1.0
-        s = QUBOTools.SampleSet(
-            QUBOTools.Sample{Float64,Int}[
-                QUBOTools.Sample([0, 0, 1], 1.0, 1),
-                QUBOTools.Sample([0, 1, 0], 2.0, 2),
-                QUBOTools.Sample([0, 1, 1], 3.0, 3),
-                QUBOTools.Sample([1, 0, 0], 4.0, 4),
-            ],
-            Dict{String,Any}(
-                "time" => Dict{String,Any}("total" => 2.0, "effective" => 1.0),
-            ),
-        )
+        test_solution_metrics()
+        test_model_metrics()
+    end
 
-        @testset "⋅ Time-to-Target" begin
-            @test QUBOTools.total_time(s) == 2.0
-            @test QUBOTools.effective_time(s) == 1.0
+    return nothing
+end
 
-            @test QUBOTools.success_rate(s, e) ≈ 0.1 atol = 1e-8
-            @test QUBOTools.ttt(s, e) ≈ 43.708690653 atol = 1e-8
+function test_solution_metrics()
+    @testset "⋅ Solution" begin
+        @testset "Time-to-Target" begin
+            let λ = 1.0,
+                sol = QUBOTools.SampleSet(
+                    QUBOTools.Sample{Float64,Int}[
+                        QUBOTools.Sample([0, 0, 1], 1.0, 1),
+                        QUBOTools.Sample([0, 1, 0], 2.0, 2),
+                        QUBOTools.Sample([0, 1, 1], 3.0, 3),
+                        QUBOTools.Sample([1, 0, 0], 4.0, 4),
+                    ],
+                    Dict{String,Any}(
+                        "time" => Dict{String,Any}("total" => 2.0, "effective" => 1.0),
+                    ),
+                )
 
-            let s = QUBOTools.SampleSet()
-                @test isnan(QUBOTools.total_time(s))
-                @test isnan(QUBOTools.effective_time(s))
-                @test isnan(QUBOTools.success_rate(s, 0.0))
-                @test isnan(QUBOTools.ttt(s, 0.0))
+                @test QUBOTools.total_time(sol) == 2.0
+                @test QUBOTools.effective_time(sol) == 1.0
+
+                @test QUBOTools.success_rate(sol, λ) ≈ 0.1 atol = 1e-8
+                @test QUBOTools.ttt(sol, λ) ≈ 43.708690653 atol = 1e-8
             end
 
-            let s = QUBOTools.SampleSet(
+            let λ = 0.0, sol = QUBOTools.SampleSet()
+                @test isnan(QUBOTools.total_time(sol))
+                @test isnan(QUBOTools.effective_time(sol))
+                @test isnan(QUBOTools.success_rate(sol, λ))
+                @test isnan(QUBOTools.ttt(sol, λ))
+            end
+
+            let λ = 0.0,
+                sol = QUBOTools.SampleSet(
                     QUBOTools.Sample{Float64,Int}[],
                     Dict{String,Any}("time" => Dict{String,Any}()),
                 )
-                @test isnan(QUBOTools.total_time(s))
-                @test isnan(QUBOTools.effective_time(s))
-                @test isnan(QUBOTools.success_rate(s, 0.0))
-                @test isnan(QUBOTools.ttt(s, 0.0))
+
+                @test isnan(QUBOTools.total_time(sol))
+                @test isnan(QUBOTools.effective_time(sol))
+                @test isnan(QUBOTools.success_rate(sol, λ))
+                @test isnan(QUBOTools.ttt(sol, λ))
             end
 
-            let s = QUBOTools.SampleSet(
+            let λ = 0.0,
+                sol = QUBOTools.SampleSet(
                     QUBOTools.Sample{Float64,Int}[],
                     Dict{String,Any}("time" => Dict{String,Any}("total" => 1.0)),
                 )
-                @test QUBOTools.total_time(s) == 1.0
-                @test QUBOTools.effective_time(s) == 1.0
-                @test isnan(QUBOTools.success_rate(s, 0.0))
-                @test isnan(QUBOTools.ttt(s, 0.0))
+
+                @test QUBOTools.total_time(sol) == 1.0
+                @test QUBOTools.effective_time(sol) == 1.0
+                @test isnan(QUBOTools.success_rate(sol, λ))
+                @test isnan(QUBOTools.ttt(sol, λ))
             end
         end
 
-        @testset "⋅ Hamming Distance" begin
+        @testset "Hamming Distance" begin
             @test QUBOTools.hamming_distance([0, 1, 1], [0, 1, 1]) == 0
             @test QUBOTools.hamming_distance([0, 0, 1], [0, 1, 1]) == 1
             @test QUBOTools.hamming_distance([0, 0, 1], [0, 1, 0]) == 2
             @test QUBOTools.hamming_distance([1, 0, 1], [0, 1, 0]) == 3
+        end
+    end
+
+    return nothing
+end
+
+function test_model_metrics()
+    @testset "⋅ Model" begin
+        let m = QUBOTools.Model{Int,Float64,Int}(
+                Dict{Int,Float64}(1 => 0.5, 2 => 2.0, 3 => -3.0, 4 => 0.0),
+                Dict{Tuple{Int,Int},Float64}(
+                    (1, 2) => 2.0,
+                    (1, 3) => -2.0,
+                    (2, 3) => 0.5,
+                    (3, 4) => 0.0,
+                ),
+            )
+
+            @test QUBOTools.linear_density(m) ≈ 3 / 4 atol = 1e-8
+            @test QUBOTools.quadratic_density(m) ≈ 3 / 6 atol = 1e-8
+            @test QUBOTools.density(m) ≈ 9 / 16 atol = 1e-8
+        end
+
+        let m = QUBOTools.Model{Int,Float64,Int}(
+                Dict{Int,Float64}(),
+                Dict{Tuple{Int,Int},Float64}(
+                    (1, 2) => 0.0,
+                    (1, 3) => -0.0,
+                    (2, 3) => 0.0,
+                    (3, 4) => 0.0,
+                ),
+            )
+
+            @test QUBOTools.linear_density(m) ≈ 0.0 atol = 1e-8
+            @test QUBOTools.quadratic_density(m) ≈ 0.0 atol = 1e-8
+            @test QUBOTools.density(m) ≈ 0.0 atol = 1e-8
+        end
+
+        let m = QUBOTools.Model{Int,Float64,Int}(
+                Dict{Int,Float64}(1 => 0.5, 2 => 2.0, 3 => -3.0, 4 => 1.0),
+                Dict{Tuple{Int,Int},Float64}(
+                    (1, 2) => 2.0,
+                    (1, 3) => -2.0,
+                    (1, 4) => 3.0,
+                    (2, 3) => 0.5,
+                    (2, 4) => -1.0,
+                    (3, 4) => 1.0,
+                )
+            )
+
+            @test QUBOTools.linear_density(m) ≈ 1.0 atol = 1e-8
+            @test QUBOTools.quadratic_density(m) ≈ 1.0 atol = 1e-8
+            @test QUBOTools.density(m) ≈ 1.0 atol = 1e-8
         end
     end
 
