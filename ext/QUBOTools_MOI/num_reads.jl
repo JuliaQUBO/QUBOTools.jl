de8f6b7d969c1741fe36e914051dfe5a196e8e06
pedromxavier <pedromxavier@poli.ufrj.br>
2023-12-04 13:01:08 -0500

    Restrict scope for NumberOfReads attribute

--- ext/QUBOTools_MOI/num_reads.jl
+++ ext/QUBOTools_MOI/num_reads.jl
@@ -7,24 +7,14 @@ struct NumberOfReads <: MOI.AbstractModelAttribute
     result_index::Int
 
     function NumberOfReads(result_index::Integer = 1)
+        @assert result_index > 0
+
         new(result_index)
     end
 end
 
 function MOI.is_set_by_optimize(::NumberOfReads)
     return true
-end
-
-function MOI.supports(::MOI.ModelLike, ::NumberOfReads)
-    return true
-end
-
-function MOI.get(model::MOI.ModelLike, attr::NumberOfReads)
-    MOI.check_result_index_bounds(model, attr)
-
-    # By default, solvers that do not implement their own method
-    # will not account for solution multiplicity.
-    return 1
 end
 
 # The function takes no arguments and returns the NumberOfReads type.
